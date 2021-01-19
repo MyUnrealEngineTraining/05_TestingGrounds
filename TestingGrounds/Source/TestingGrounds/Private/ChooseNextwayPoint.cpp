33905f3db959d7430889c97d69f12ebbf9acc25e
Yurii <predbannikov.yurii@gmail.com>
2021-01-20 00:55:20 +0300

    211

--- TestingGrounds/Source/TestingGrounds/Private/ChooseNextwayPoint.cpp
+++ TestingGrounds/Source/TestingGrounds/Private/ChooseNextwayPoint.cpp
@@ -4,10 +4,16 @@
 #include "ChooseNextwayPoint.h"
 
 
+
+#include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BehaviorTreeTypes.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextwayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Task"))
+	const auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index = %i"), Index)
+	
 	return EBTNodeResult::Succeeded;
 }
--- TestingGrounds/Source/TestingGrounds/Public/ChooseNextwayPoint.h
+++ TestingGrounds/Source/TestingGrounds/Public/ChooseNextwayPoint.h
@@ -15,4 +15,8 @@ class TESTINGGROUNDS_API UChooseNextwayPoint : public UBTTaskNode
 	GENERATED_BODY()
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Bkackboard")
+	struct FBlackboardKeySelector IndexKey;
+	
 };
